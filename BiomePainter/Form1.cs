116d4cc09da06775b833ba473d0bed43719d02e3
Matthew Blaine <mblaine@ymail.com>
2012-03-29 21:13:44 -0400

    Removed unused using statements.

--- BiomePainter/Form1.cs
+++ BiomePainter/Form1.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
