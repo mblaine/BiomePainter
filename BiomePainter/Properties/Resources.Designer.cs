40f8c98b6b2d2e02e2a58835ef2012cbee56619c
Matthew Blaine <mblaine@ymail.com>
2012-04-03 00:39:33 -0400

    Reorganizing images.

--- BiomePainter/Properties/Resources.Designer.cs
+++ BiomePainter/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BiomePainter.Properties
-{
-
-
+namespace BiomePainter.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
