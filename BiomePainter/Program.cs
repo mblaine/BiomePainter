8f84bdd710b982307f6ed53504aa4c1f682fdf74
Matthew Blaine <mblaine@ymail.com>
2012-05-27 22:50:02 -0400

    Exception logging.

--- BiomePainter/Program.cs
+++ BiomePainter/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace BiomePainter
@@ -11,9 +17,62 @@ namespace BiomePainter
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledException;
+            Application.ThreadException += ThreadException;
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception)
+                LogException((Exception)e.ExceptionObject);
+        }
+
+        static void ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            LogException(e.Exception);
+        }
+
+        static void LogException(Exception ex)
+        {
+            try
+            {
+                DateTime dt = DateTime.Now;
+                String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "BiomePainter", String.Format("exception-{0:yyyy-MM-dd-HH-mm-ss}.txt", dt));
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendFormat("{0} version {1} - {2:M/d/yyy h:mm:ss tt}{3}{3}", Assembly.GetExecutingAssembly().Location, FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion, dt, Environment.NewLine);
+
+                    Regex pattern = new Regex(@"^(.* in )(.*)(\:line.*)$");
+                    String[] lines = ex.ToString().Split('\r', '\n');
+                    foreach (String line in lines)
+                    {
+                        if (line.Length > 0)
+                        {
+                            //remove full paths from trace
+                            Match m = pattern.Match(line);
+                            if (m.Success)
+                                sb.AppendFormat("{0}{1}{2}{3}", m.Groups[1].Value, Path.GetFileName(m.Groups[2].Value), m.Groups[3].Value, Environment.NewLine);
+                            else
+                                sb.AppendLine(line);
+                        }
+                    }
+                    
+                    sw.Write(sb.ToString());
+                    sw.Close();
+                }
+                
+                MessageBox.Show(null, String.Format("Biome Painter has encountered an unexpected error. The log has been saved to \"{0}\". To open the log, click \"Help\". Otherwise click \"OK\" to continue.", path), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, 0, path);
+            }
+            catch (Exception e)
+            {
+                StackTrace st = new StackTrace(e, false);
+                MessageBox.Show("Error saving error log: " + st.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
