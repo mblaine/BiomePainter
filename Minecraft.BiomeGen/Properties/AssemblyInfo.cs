52b7bb2955f2238e06291fb3aa30f9b7ba90f208
Matthew Blaine <mblaine@ymail.com>
2012-09-08 19:34:43 -0400

    Combining projects.

--- Minecraft.BiomeGen/Properties/AssemblyInfo.cs
+++ Minecraft.BiomeGen/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Minecraft Beta 1.7.3")]
+[assembly: AssemblyTitle("Minecraft Biome Generation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2959d47-8588-48f9-8d2c-540ec5931df9")]
+[assembly: Guid("914c9d79-8e08-4806-8d09-aa7857f5739f")]
 
 // Version information for an assembly consists of the following four values:
 //
